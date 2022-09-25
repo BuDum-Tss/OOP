c8e54e2895395767aea49825846fa2b9366df1ab
BaDum-Tss <99732685+BuDum-Tss@users.noreply.github.com>
2022-09-25 15:34:06 +0700

    Changed asserts

--- task-1-1-1/lib/src/test/java/ru/nsu/fit/apotapova/heapsort/HeapSortTest.java
+++ task-1-1-1/lib/src/test/java/ru/nsu/fit/apotapova/heapsort/HeapSortTest.java
@@ -1,6 +1,5 @@
 package ru.nsu.fit.apotapova.heapsort;
-// Assertions
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static ru.nsu.fit.apotapova.heapsort.HeapSort.heapSort;
 
@@ -12,42 +11,42 @@ class HeapSortTest {
       int[] answer = {1, 2, 3, 4, 5};
       int k = array.length;
       heapSort(array);
-      for (int i = 0; i < k; i++) assert (array[i] == answer[i]);
+      Assertions.assertNotEquals(answer,array);
     }
     {
       int[] array = {1, 2, 3, 4, 5};
       int[] answer = {1, 2, 3, 4, 5};
       int k = array.length;
       heapSort(array);
-      for (int i = 0; i < k; i++) assert (array[i] == answer[i]);
+      Assertions.assertNotEquals(answer,array);
     }
     {
       int[] array = {5, 4, 3, 2, 1};
       int[] answer = {1, 2, 3, 4, 5};
       int k = array.length;
       heapSort(array);
-      for (int i = 0; i < k; i++) assert (array[i] == answer[i]);
+      Assertions.assertNotEquals(answer,array);
     }
     {
       int[] array = {-1, -1, -1, -1};
       int[] answer = {-1, -1, -1, -1};
       int k = array.length;
       heapSort(array);
-      for (int i = 0; i < k; i++) assert (array[i] == answer[i]);
+      Assertions.assertNotEquals(answer,array);
     }
     {
       int[] array = {-5, -3, -4, -2, -1};
       int[] answer = {-5, -4, -3, -2, -1};
       int k = array.length;
       heapSort(array);
-      for (int i = 0; i < k; i++) assert (array[i] == answer[i]);
+      Assertions.assertNotEquals(answer,array);
     }
     {
       int[] array = {};
       int[] answer = {};
       int k = array.length;
       heapSort(array);
-      for (int i = 0; i < k; i++) assert (array[i] == answer[i]);
+      Assertions.assertNotEquals(answer,array);
     }
   }
 }
