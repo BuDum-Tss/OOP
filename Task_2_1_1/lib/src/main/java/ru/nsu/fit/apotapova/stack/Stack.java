c8150dadf822b13b3263ab64bcf52c46839a8fa2
BaDum-Tss <99732685+BuDum-Tss@users.noreply.github.com>
2022-10-02 21:55:06 +0700

    codeStyle

--- Task_2_1_1/lib/src/main/java/ru/nsu/fit/apotapova/stack/Stack.java
+++ Task_2_1_1/lib/src/main/java/ru/nsu/fit/apotapova/stack/Stack.java
@@ -1,22 +1,23 @@
 package ru.nsu.fit.apotapova.stack;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-
-/** Class Stack contains methods for working with this data storage method
+import org.jetbrains.annotations.NotNull;
+/**
+ * Class Stack contains methods for working with this data storage method.
  *
  * @param <T> - data type
  */
+
 public class Stack<T> {
   int size = 32;
   private final T[] stack = (T[]) new Object[size];
   private int top = -1;
 
-  /** add an element to the stack
+  /**
+   * add an element to the stack.
    *
    * @param element - element
    */
@@ -24,17 +25,21 @@ public class Stack<T> {
     stack[++top] = element;
   }
 
-  /** add collection of elements to the stack
+  /**
+   * add collection of elements to the stack.
    *
    * @param elements - collection of elements
    */
   public void pushStack(@NotNull Collection<T> elements) {
     int n = elements.size();
     T[] array = (T[]) elements.toArray();
-    while (n-- > 0) stack[++top] = array[n];
+    while (n-- > 0) {
+      stack[++top] = array[n];
+    }
   }
 
-  /** take an element from the stack
+  /**
+   * take an element from the stack.
    *
    * @return - element
    */
@@ -42,9 +47,10 @@ public class Stack<T> {
     return stack[top--];
   }
 
-  /** take n elements from the stack
+  /**
+   * take n elements from the stack.
    *
-   * @param n -  number of elements
+   * @param n - number of elements
    * @return - stack of elements
    */
   public Stack<T> popStack(int n) {
@@ -55,7 +61,8 @@ public class Stack<T> {
     return container;
   }
 
-  /** count the number of elements in the stack
+  /**
+   * count the number of elements in the stack.
    *
    * @return quantity of elements
    */
@@ -63,7 +70,8 @@ public class Stack<T> {
     return top + 1;
   }
 
-  /** converts stack to list
+  /**
+   * converts stack to list.
    *
    * @return List
    */
