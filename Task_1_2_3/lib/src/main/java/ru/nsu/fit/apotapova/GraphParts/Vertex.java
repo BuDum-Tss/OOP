d9deb9ef22f967358f5c4ce7601ec5580811af51
BaDum-Tss <99732685+BuDum-Tss@users.noreply.github.com>
2022-11-28 16:50:52 +0700

    error

---
+++ Task_1_2_3/lib/src/main/java/ru/nsu/fit/apotapova/GraphParts/Vertex.java
@@ -0,0 +1,5 @@
+package ru.nsu.fit.apotapova.GraphParts;
+
+public class Vertex<T> {
+  T value;
+}
---
+++ Task_1_2_3/lib/src/main/java/ru/nsu/fit/apotapova/Graphs/IncidenceMatrixGraph.java
@@ -0,0 +1,5 @@
+package ru.nsu.fit.apotapova.Graphs;
+
+public class IncidenceMatrixGraph<T ,Number> implements Graph<T ,Number>{
+
+}
