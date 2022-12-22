86bbba293179be7cdcb7bf825a2ed6950c510407
BuDum-Tss <BuDum-Tss@users.noreply.github.com>
2022-12-22 19:21:52 +0000

    Deploying to gh-pages from @ BuDum-Tss/OOP@8ea169865b0e705b94f8e9404688db4b2e5153f0 🚀

--- Task_1_2_3/package-search-index.js
+++ Task_1_2_3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.fit.apotapova"},{"l":"ru.fit.apotapova.GraphParts"},{"l":"ru.fit.apotapova.GraphTypes"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.fit.apotapova"},{"l":"ru.fit.apotapova.Alhoritms"},{"l":"ru.fit.apotapova.GraphParts"},{"l":"ru.fit.apotapova.GraphTypes"}]
